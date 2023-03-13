743694542640a083223092694676cefd503bf592
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2023-03-13 13:48:27 -0500

    Add resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,30 +1,94 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import LinkButton from "../components/LinkButton";
 import Layout from "../components/Layout";
 
-const Home: NextPage = () => {
+const Resume: NextPage = () => {
   return (
     <Layout>
       <Head>
-        <title>About</title>
+        <title>Resume</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className="text-6xl font-bold">Resume</h1>
-
-      <p className="mt-3 text-2xl">
-        About{" "}
-        <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-          pages/index.tsx
-        </code>
-      </p>
-
-      <div className="mt-6 flex max-w-4xl flex-col items-center justify-around sm:w-full">
-        <LinkButton link={"https://nextjs.org/learn"} text={"About"} />
+      <div className={"flex flex-col text-left gap-6"}>
+        <div>
+          <h3 className="text-3xl font-bold">Software Engineer - ConGenius</h3>
+          <ul className="mt-3 text-2xl">
+            <li>
+              Develop core application functionality in the beginning stages of
+              the startup
+            </li>
+            <li>Build and consume REST endpoints in our application</li>
+            <li>Mentor younger developers and keep timelines in check</li>
+            <li>Ensure code quality across the team through code reviews</li>
+            <li>
+              Technologies: React, Docker, Node.js, TypeScript, PostgreSQL
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-3xl font-bold">
+            Software Engineer - Digital Monitoring Products
+          </h3>
+          <ul className="mt-3 text-2xl">
+            <li>
+              Develop full-stack web applications from designing databases,
+              building REST and GraphQL APIs, all the way to building the
+              front-end applications using React.
+            </li>
+            <li>
+              Design and manage features from development to testing and
+              delivery.
+            </li>
+            <li>
+              Advocate for user experience improvement for our apps which have
+              100,000+ users.
+            </li>
+            <li>
+              Build GraphQL application using Apollo Server to standardize a
+              diverse set of REST APIs.
+            </li>
+            <li>
+              Technologies: React, GraphQL, Node.js, TypeScript, SQL Server
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-3xl font-bold">
+            Web Developer - Missouri State Office of Web Strategy and
+            Development
+          </h3>
+          <ul className="mt-3 text-2xl">
+            <li>Full-stack, waterfall environment</li>
+            <li>
+              Update websites and web applications. Fix bugs, patch security
+              vulnerabilities, developed new features, and up-date legacy web
+              pages to modern standards.
+            </li>
+            <li>
+              Technologies: JavaScript, jQuery, Classic ASP, Microsoft SQL
+              Server
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-3xl font-bold">
+            Software Development Intern - O'Reilly Auto Parts, Inc.
+          </h3>
+          <ul className="mt-3 text-2xl">
+            <li>Full-stack, waterfall environment</li>
+            <li>
+              Develop application to centralize documents and manage progress of
+              product development.
+            </li>
+            <li>
+              Technologies: Java, Spring Framework, JavaScript, Bootstrap,
+              Elasticsearch
+            </li>
+          </ul>
+        </div>
       </div>
     </Layout>
   );
 };
 
-export default Home;
+export default Resume;
