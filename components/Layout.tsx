1ecb08db0ddc5dc878a3af393579f2a6b7c7d932
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2023-03-13 14:17:35 -0500

    Fix layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,11 +7,11 @@ function Layout({ children }: PropsWithChildren) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Link href={"/"}>
-        <div className="flex h-24 w-screen items-center justify-center border-b">
-          <h1 className="text-6xl font-bold">Luke Johnson</h1>
+        <div className="flex h-24 w-screen items-center justify-center border-b sticky top-0">
+          <h1 className="text-5xl lg:text-6xl font-bold">Luke Johnson</h1>
         </div>
       </Link>
-      <main className="flex w-full flex-1 flex-col items-center justify-center lg:px-20 text-center bg-off-white">
+      <main className="relative flex w-full flex-1 flex-col items-center justify-center lg:px-20 text-center bg-off-white">
         {children}
       </main>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="mt-6 flex max-w-4xl flex-col items-center justify-around sm:w-full">
+      <div className="mt-6 flex max-w-4xl flex-col items-center justify-around sm:w-full p-3">
         <LinkButton.Purple link={"/about"} text={"About"} />
         <LinkButton.Pink link={"/resume"} text={"Resume"} />
         <LinkButton.Purple link={"/contact"} text={"Contact"} />
