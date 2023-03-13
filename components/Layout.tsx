67f372c115f452093144017467ed8f121f225d63
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2023-03-13 13:53:52 -0500

    Make header link to home

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,15 @@
 import { PropsWithChildren } from "react";
 import Head from "next/head";
 import LinkButton from "./LinkButton";
+import Link from "next/link";
 
 function Layout({ children }: PropsWithChildren) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <head className="flex h-24 w-screen items-center justify-center border-b">
-        <h1 className="text-6xl font-bold">Luke Johnson</h1>
+        <Link href={"/"}>
+          <h1 className="text-6xl font-bold">Luke Johnson</h1>
+        </Link>
       </head>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center bg-off-white">
         {children}
