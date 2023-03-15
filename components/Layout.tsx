3eac733ccabbe29672f318eaf1b792747e52e27e
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2023-03-15 18:26:10 -0500

    fix layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,12 +6,15 @@ import Link from "next/link";
 function Layout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col min-h-screen ">
-      <Link href={"/"}>
-        <div className="flex h-24 w-full items-center justify-center border-b">
-          <h1 className="text-5xl lg:text-6xl font-bold">Luke Johnson</h1>
-        </div>
+      <Link
+        href={"/"}
+        className={
+          "flex h-24 w-full items-center justify-center border-b sticky top-0 z-50 bg-off-white"
+        }
+      >
+        <h1 className="text-5xl lg:text-6xl font-bold">Luke Johnson</h1>
       </Link>
-      <div className="flex flex-grow flex-col items-center justify-center">
+      <div className="relative flex flex-grow flex-col items-center justify-center">
         {children}
       </div>
 
