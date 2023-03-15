eeb6cae369b50809f819ee18a649001658c6f377
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2023-03-15 18:20:17 -0500

    Fix footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 
 function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="grid-rows-[auto_1fr_auto] grid-cols-[auto_1fr_auto]">
+    <div className="flex flex-col min-h-screen ">
       <Link href={"/"}>
         <div className="flex h-24 w-full items-center justify-center border-b">
           <h1 className="text-5xl lg:text-6xl font-bold">Luke Johnson</h1>
         </div>
       </Link>
-      <div className="flex w-full h-full flex-col items-center justify-center  ">
+      <div className="flex flex-grow flex-col items-center justify-center">
         {children}
       </div>
 
-      <footer className="flex h-24 w-screen items-center justify-center border-t ">
+      <footer className="flex h-24 w-screen mt-auto items-center justify-center border-t">
         Ⓒ2023 Luke Johnson
       </footer>
     </div>
